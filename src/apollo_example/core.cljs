078e6c6fbee0edf3ede1345a6b9a8de3e601bd65
Will Acton <will@amperity.com>
2020-01-20 11:41:38 -0800

    migrate to helix, apollo-hooks, and upgrade everything

--- src/apollo_example/core.cljs
+++ src/apollo_example/core.cljs
@@ -1,9 +1,10 @@
 (ns apollo-example.core
-  (:require [hx.react :as hx :refer [defnc]]
+  (:require [helix.core :refer [defnc $]]
+            [helix.dom :as d]
             ["react-dom" :as rdom]
-            ["react-apollo" :as apollo]
             ["apollo-boost" :default ApolloClient :refer [gql]]
-            [applied-science.js-interop :as j]))
+            ["@apollo/react-hooks" :as apollo :refer [useQuery]]
+            [cljs-bean.core :as b]))
 
 (def client (ApolloClient. #js {:uri "https://48p1r2roz4.sse.codesandbox.io"}))
 
@@ -15,23 +16,22 @@
                            }"))
 
 (defnc ExchangeRates [_]
-  [apollo/Query {:query exchange-query :asdf-jkl "foo"}
-   (fn [result]
-     ;; For some reason, react-apollo results won't conver to CLJS data cleanly
-     ;; Possibly some weird prototype thing.
-     ;; So, we use the js-interop library for easy access to the result object
-     (let [{:keys [loading error data]} (j/lookup result)]
-       (cond
-         loading [:div "Loading..."]
-         error [:div "Error :("]
-         :else [:div (for [entry (j/get data :rates)]
-                       (let [{:keys [currency rate]} (j/lookup entry)]
-                         [:div {:key currency}
-                          [:p currency ": " rate]]))])))])
+  (let [{:keys [loading error data]} (-> (useQuery exchange-query)
+                                         ;; wrap result in a bean to destructure
+                                         (b/bean :recursive true))]
+    (cond
+      loading (d/div "Loading...")
+      error (d/div "Error :(")
+      :else (d/div "Success"
+             (for [entry (:rates data)]
+               (let [{:keys [currency rate]} entry]
+                 (d/div {:key currency}
+                        (d/p currency ": " rate))))))))
 
 (defnc App [_]
-  [apollo/ApolloProvider {:client client}
-   [:div
-    [ExchangeRates]]])
+  ($ apollo/ApolloProvider
+     {:client client}
+     (d/div ($ ExchangeRates))))
 
-(rdom/render (hx/f [App]) (.getElementById js/document "app"))
+(defn ^:dev/after-load start []
+  (rdom/render ($ App) (.getElementById js/document "app")))
